bf5e146cd719cc2b024479c24cfe95d98326e33e
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 13:05:06 +0530

    Update PriceConsumerV3_unit_test.js

--- test/unit/PriceConsumerV3_unit_test.js
+++ test/unit/PriceConsumerV3_unit_test.js
@@ -5,7 +5,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("PriceConsumer Unit Tests", async function () {
-      // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+      // Pricefeed addresses can be obtained at https://docs.chain.link/docs/reference-contracts
       let priceConsumerV3, mockV3Aggregator
 
       beforeEach(async () => {
