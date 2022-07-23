e3af100beb81bb98a3947d9cda553c617c19f4c1
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 13:04:05 +0530

    Update RandomNumberConsumerV2_int_test.js

--- test/staging/RandomNumberConsumerV2_int_test.js
+++ test/staging/RandomNumberConsumerV2_int_test.js
@@ -16,16 +16,18 @@ developmentChains.includes(network.name)
       })
 
       // We can't use an arrow functions here because we need to use `this`. So we need
-      // to use `async function() {` as seen.
+      // to use `async function() {` as below
       it("Our event should successfully fire event on callback", async function () {
-        this.timeout(300000) // wait 300 seconds max
-        // we setup a promise so we can wait for our callback from the `once` function
+        this.timeout(300000) // Waits 300 seconds max
+        
+        // We setup a promise so we can wait for our callback from the `once` function
         await new Promise(async (resolve, reject) => {
-          // setup listener for our event
+          // Setup listener for our event
           randomNumberConsumerV2.once("ReturnedRandomness", async () => {
             console.log("ReturnedRandomness event fired!")
             const firstRandomNumber = await randomNumberConsumerV2.s_randomWords(0)
             const secondRandomNumber = await randomNumberConsumerV2.s_randomWords(1)
+            
             // assert throws an error if it fails, so we need to wrap
             // it in a try/catch so that the promise returns event
             // if it fails.
