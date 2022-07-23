6aa4d58fb5a6f06e2bca61db06fbc12fb9a26711
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 12:58:40 +0530

    Update read-data.js

--- tasks/api-consumer/read-data.js
+++ tasks/api-consumer/read-data.js
@@ -6,17 +6,19 @@ task("read-data", "Calls an API Consumer Contract to read data obtained from an 
     console.log("Reading data from API Consumer contract ", contractAddr, " on network ", networkId)
     const APIConsumer = await ethers.getContractFactory("APIConsumer")
 
-    //Get signer information
+    // Get signer information
     const accounts = await ethers.getSigners()
     const signer = accounts[0]
 
-    //Create connection to API Consumer Contract and call the createRequestTo function
+    // Create connection to API Consumer Contract and call the createRequestTo function
     const apiConsumerContract = new ethers.Contract(contractAddr, APIConsumer.interface, signer)
     let result = BigInt(await apiConsumerContract.volume()).toString()
     console.log("Data is: ", result)
+  
     if (result == 0 && ["hardhat", "localhost", "ganache"].indexOf(network.name) == 0) {
       console.log("You'll either need to wait another minute, or fix something!")
     }
+  
     if (["hardhat", "localhost", "ganache"].indexOf(network.name) >= 0) {
       console.log("You'll have to manually update the value since you're on a local chain!")
     }
