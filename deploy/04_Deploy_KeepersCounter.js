e52e871a01e21d5797f92dc34c88916991630567
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 12:57:37 +0530

    Update 04_Deploy_KeepersCounter.js

--- deploy/04_Deploy_KeepersCounter.js
+++ deploy/04_Deploy_KeepersCounter.js
@@ -11,22 +11,27 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
   const keepersUpdateInterval = networkConfig[chainId]["keepersUpdateInterval"] || "30"
-  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
-  // Default one below is ETH/USD contract on Kovan
+  
+  // Pricefeed addresses can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one below is ETH / USD contract on Kovan
   const waitBlockConfirmations = developmentChains.includes(network.name)
     ? 1
     : VERIFICATION_BLOCK_CONFIRMATIONS
+  
   const args = [keepersUpdateInterval]
+  
   const keepersCounter = await deploy("KeepersCounter", {
     from: deployer,
     args: args,
     log: true,
     waitConfirmations: waitBlockConfirmations,
   })
+  
   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
     log("Verifying...")
     await verify(keepersCounter.address, args)
   }
+  
   log(
     "Head to https://keepers.chain.link/ to register your contract for upkeeps. Then run the following command to track the counter updates: "
   )
