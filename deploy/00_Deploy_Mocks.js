fefc1f0def0c060ce83cdca055eb764937aea250
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 12:53:47 +0530

    Update 00_Deploy_Mocks.js

--- deploy/00_Deploy_Mocks.js
+++ deploy/00_Deploy_Mocks.js
@@ -8,16 +8,19 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
-  // If we are on a local development network, we need to deploy mocks!
+  
+  // If we are on a local development network, we need to deploy mocks
   if (chainId == 31337) {
     log("Local network detected! Deploying mocks...")
     const linkToken = await deploy("LinkToken", { from: deployer, log: true })
+    
     await deploy("MockV3Aggregator", {
       contract: "MockV3Aggregator",
       from: deployer,
       log: true,
       args: [DECIMALS, INITIAL_PRICE],
     })
+    
     await deploy("VRFCoordinatorV2Mock", {
       from: deployer,
       log: true,
@@ -26,11 +29,13 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         1e9, // 0.000000001 LINK per gas
       ],
     })
+    
     await deploy("MockOracle", {
       from: deployer,
       log: true,
       args: [linkToken.address],
     })
+    
     log("Mocks Deployed!")
     log("----------------------------------------------------")
     log("You are deploying to a local network, you'll need a local network running to interact")
@@ -38,4 +43,5 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     log("----------------------------------------------------")
   }
 }
+
 module.exports.tags = ["all", "mocks", "main"]
